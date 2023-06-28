2ba56226eeabe56162958020da425dbada8fb5dd
Spencer Chang <spencerc99@gmail.com>
2023-06-28 14:11:25 -0700

    enable testing infra

---
+++ test/test.spec.ts
@@ -0,0 +1,11 @@
+import { assert } from "chai";
+
+const testBulletedLists = [
+  `- i am
+  - i am an expanding
+    - i am an expanding list`,
+];
+
+describe("Telescopic Text", () => {
+  it("should work on a simple example", () => {});
+});
